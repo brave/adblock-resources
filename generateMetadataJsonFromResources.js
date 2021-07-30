3cb3a7dc31dfefb1736532f4f755ad7cca28386a
Shivan Kaul Sahib <shivankaulsahib@gmail.com>
2021-07-30 16:20:49 -0700

    [WIP] SugarCoat resources

---
+++ generateMetadataJsonFromResources.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
+const metadataJsonFile = "metadata.json";
+const resourcesDir = "resources";
+
+console.debug(`Creating ${metadataJsonFile}...`);
+const resources = fs.readdirSync(resourcesDir);
+const metadataList = resources.map((file) => {
+  return {
+    name: path.basename(file, ".js"),
+    aliases: [path.basename(file, ".js")],
+    kind: { mime: "application/javascript" },
+    resourcePath: path.basename(file),
+  };
+});
+
+console.debug(`Writing ${metadataJsonFile}... `);
+// Pretty print out
+fs.writeFileSync(metadataJsonFile, JSON.stringify(metadataList, null, 4));
