cf84b96310f30f1dbb97e50304d898b22a9d3e7a
Shivan Kaul Sahib <shivankaulsahib@gmail.com>
2022-01-05 13:04:54 +0530

    Re-add alias

--- generateMetadataJsonFromScriptResources.js
+++ generateMetadataJsonFromScriptResources.js
@@ -1,3 +1,9 @@
+/**
+ * Helper script that takes resources in /resources and generates the metadata.json
+ * Note that this does not add the alias - please edit those manually. 
+ * For instance - brave-fix script needs the "bf" alias
+ */
+
 const fs = require("fs");
 const path = require("path");
 
@@ -18,3 +24,4 @@ const metadataList = resources.map((file) => {
 console.debug(`Writing ${metadataJsonFile}... `);
 // Pretty print out
 fs.writeFileSync(metadataJsonFile, JSON.stringify(metadataList, null, 4));
+console.debug(`Done! IMPORTANT: Please edit ${metadataJsonFile} to have the right aliases for the resources`);
