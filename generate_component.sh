67ce8d9899b6e65bda2b830efc38085003c6c075
Anton Lazarev <antonok35@gmail.com>
2022-08-29 23:11:28 -0700

    add generate_component script

---
+++ generate_component.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/sh
+
+# See the README for more information about this script.
+
+TEMPFILE="$(mktemp)"
+
+openssl genrsa 2048 2>/dev/null | openssl pkcs8 -topk8 -nocrypt -out $TEMPFILE
+PUBKEY=$(openssl rsa -in $TEMPFILE -pubout -outform DER 2>/dev/null | openssl base64 -A)
+COMPONENTID=$(openssl rsa -in $TEMPFILE -pubout -outform DER 2>/dev/null | shasum -a 256 | head -c32 | tr 0-9a-f a-p)
+
+PEMFILE="ad-block-updater-$COMPONENTID.pem"
+
+echo "$PUBKEY"
+echo
+echo "$COMPONENTID"
+echo
+
+mv "$TEMPFILE" "$PEMFILE"
+
+echo "Private key written to $PEMFILE."
+echo "Please upload it to 1Password and make sure it is synced with Jenkins."
