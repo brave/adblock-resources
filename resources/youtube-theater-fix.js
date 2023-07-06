8b3914da9a18de1029c6db1a7a3c8f877b20e30b
pes10k <76526+pes10k@users.noreply.github.com>
2023-07-06 14:26:29 -0500

    Create youtube-theater-fix.js, fixes brave/brave-browser#29591
    adds a scriptlet to mirror the session cookie value in localstorage

---
+++ resources/youtube-theater-fix.js
@@ -0,0 +1,20 @@
+(async _ => {
+    if (self.cookieStore === undefined) {
+        // do nothing on iOS. The problem doesn't exist there, and the
+        // solution wouldn't work anyway
+        return
+    }
+    const storeKey = 'brave::wide'
+    const cookieKey = 'wide'
+    if (localStorage[storeKey] !== undefined) {
+        await cookieStore.set(cookieKey, localStorage[storeKey])
+    }
+    setInterval(_ => {
+        try {
+            const wideCookie = await cookieStore(cookieKey)
+            localStorage[storeKey] = wideCookie.value
+        } catch () {
+            // swallow error from no cookie existing
+        }
+    }, 1000)
+})();
