8ff15e9ebbbc40bdc8abbb5a8ad3a56e1147388d
ryanbr <rbrown@brave.com>
2024-08-03 23:30:14 +0000

    Modify trusted-type scriptlet

--- resources/trusted_types.js
+++ resources/trusted_types.js
@@ -1,9 +1,15 @@
 /// set-trusted-types.js
-function setTrustedTypes() {
+
+{function runAt(fn, when) {...}
+
+function safeSelf() {...}
+
+(function setTrustedTypes() {
   if (window.trustedTypes && window.trustedTypes.createPolicy) {
     window.trustedTypes.createPolicy("default", {
       createScript: string => string
     });
    }
    runAt(() => {}, 'interactive');
+}());
 }
