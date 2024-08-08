ccf55185fb9f32a6741f2dc0c996276a575b942b
tapanmodh <modhtapan@gmail.com>
2024-08-09 01:16:50 +0530

    using mutation observer

--- resources/brave-youtube-quality.js
+++ resources/brave-youtube-quality.js
@@ -2,55 +2,29 @@
 (function() {
     const IS_MOBILE_YOUTUBE = window.location.hostname == 'm.youtube.com';
     const IS_ANDROID = window.navigator.userAgent.indexOf('Android') > -1;
-
-    let intervalId = 0;    
-    if (IS_ANDROID && IS_MOBILE_YOUTUBE && typeof isYtHdQualityPlaybackEnabled !== 'undefined' && isYtHdQualityPlaybackEnabled) {
+ 
+    if (IS_ANDROID && IS_MOBILE_YOUTUBE && typeof isYoutubeHdQualityPlaybackEnabled !== 'undefined' && isYoutubeHdQualityPlaybackEnabled) {
         navigation.addEventListener('navigate', (event) => {
             const url = new URL(event.destination.url);
             if (url.pathname && url.pathname === '/watch' && url.search && url.searchParams.get('v')) {
-                handlePlaybackVideoQuality();
+                const mutationObserver = new MutationObserver(() => {
+                    let player = document.getElementById('movie_player');
+                    if (player && typeof player.getAvailableQualityLevels !== 'undefined') {
+                        setPlaybackVideoQuality(player, 'hd720');
+                        mutationObserver.disconnect();
+                    }
+                });
+                mutationObserver.observe(document.body, { childList: true, subtree: true });
             }
         });
 
         function setPlaybackVideoQuality(player, quality) {
-            if (!player || typeof player.getAvailableQualityLevels === 'undefined') {
-                return false;
+
+            if (player.setPlaybackQualityRange) {
+                player.setPlaybackQualityRange(quality);
+            } else if (player.setPlaybackQuality) {
+                player.setPlaybackQuality(quality);
             }
-
-            let availableQualities = player.getAvailableQualityLevels();
-            if (availableQualities && availableQualities.length > 0) {
-                if (player.setPlaybackQualityRange) {
-                    player.setPlaybackQualityRange(quality);
-                    return true;
-                }
-
-                if (player.setPlaybackQuality) {
-                    player.setPlaybackQuality(quality);
-                    return true;
-                }
-            }
-            
-            return false;
-        }
-
-        function handlePlaybackVideoQuality() {
-            let attemptCount = 0;
-            const maxAttempts = 5;
-            clearInterval(intervalId);
-            intervalId = setInterval(function() {
-                try {
-                    let player = document.getElementById('movie_player');
-                    if (attemptCount++ > maxAttempts) {
-                        clearInterval(intervalId);
-                        return;
-                    }
-                    if (setPlaybackVideoQuality(player, 'hd720')) {
-                        clearInterval(intervalId);
-                    }
-                } catch (error) {
-                    clearInterval(intervalId);
-                }
-            }, 500)
         }
     }
 })()
