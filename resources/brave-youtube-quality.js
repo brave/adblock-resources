087fb3f606613802a97a9ccbea15dddd339d0546
tapanmodh <modhtapan@gmail.com>
2024-08-10 00:36:16 +0530

    testing default quality

--- resources/brave-youtube-quality.js
+++ resources/brave-youtube-quality.js
@@ -3,14 +3,16 @@
     const IS_MOBILE_YOUTUBE = window.location.hostname == 'm.youtube.com';
     const IS_ANDROID = window.navigator.userAgent.indexOf('Android') > -1;
  
-    if (IS_ANDROID && IS_MOBILE_YOUTUBE && typeof isYoutubeHdQualityPlaybackEnabled !== 'undefined' && isYoutubeHdQualityPlaybackEnabled) {
+    if (IS_ANDROID && IS_MOBILE_YOUTUBE && typeof isYoutubeHdQualityPlaybackEnabled !== 'undefined') {
         navigation.addEventListener('navigate', (event) => {
             const url = new URL(event.destination.url);
             if (url.pathname && url.pathname === '/watch' && url.search && url.searchParams.get('v')) {
                 const mutationObserver = new MutationObserver(() => {
                     let player = document.getElementById('movie_player');
                     if (player && typeof player.getAvailableQualityLevels !== 'undefined') {
-                        setPlaybackVideoQuality(player, 'hd720');
+                        const quality = isYoutubeHdQualityPlaybackEnabled ? 'hd720' : 'default';
+                        console.log("tapan isYoutubeHdQualityPlaybackEnabled:"+ isYoutubeHdQualityPlaybackEnabled + ", quality:"+quality);
+                        setPlaybackVideoQuality(player, quality);
                         mutationObserver.disconnect();
                     }
                 });
