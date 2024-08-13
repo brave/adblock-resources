68a1f0738dc901482ed31a75d86df7e9242b6e4a
tapanmodh <modhtapan@gmail.com>
2024-08-13 18:57:01 +0530

    remove default quality

--- resources/brave-youtube-quality.js
+++ resources/brave-youtube-quality.js
@@ -3,16 +3,14 @@
     const IS_MOBILE_YOUTUBE = window.location.hostname == 'm.youtube.com';
     const IS_ANDROID = window.navigator.userAgent.indexOf('Android') > -1;
  
-    if (IS_ANDROID && IS_MOBILE_YOUTUBE && typeof isYoutubeHdQualityPlaybackEnabled !== 'undefined') {
+    if (IS_ANDROID && IS_MOBILE_YOUTUBE && typeof isYoutubeHdQualityPlaybackEnabled !== 'undefined' && isYoutubeHdQualityPlaybackEnabled) {
         navigation.addEventListener('navigate', (event) => {
             const url = new URL(event.destination.url);
             if (url.pathname && url.pathname === '/watch' && url.search && url.searchParams.get('v')) {
                 const mutationObserver = new MutationObserver(() => {
                     let player = document.getElementById('movie_player');
                     if (player && typeof player.getAvailableQualityLevels !== 'undefined') {
-                        const quality = isYoutubeHdQualityPlaybackEnabled ? 'hd720' : 'default';
-                        console.log("tapan isYoutubeHdQualityPlaybackEnabled:"+ isYoutubeHdQualityPlaybackEnabled + ", quality:"+quality);
-                        setPlaybackVideoQuality(player, quality);
+                        setPlaybackVideoQuality(player, 'hd720');
                         mutationObserver.disconnect();
                     }
                 });
