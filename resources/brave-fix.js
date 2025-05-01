6ea2b5eae08f7d2f0112bda514d9ebe958048fa8
ryanbr <rbrown@brave.com>
2025-05-01 05:16:07 +0000

    Combine into brave-fix

--- resources/brave-fix.js
+++ resources/brave-fix.js
@@ -2,3 +2,13 @@
 /// alias bf.js
 delete Navigator.prototype.brave
 delete window.navigator.brave
+
+const brands = navigator?.userAgentData?.brands;
+if (brands) {
+  brands.forEach((item) => {
+    if (item?.brand?.toLowerCase() === "brave") {
+      item.brand = "Google Chrome";
+    }
+  })
+  Object.defineProperty(NavigatorUAData.prototype, "brands", { value: brands });
+}
