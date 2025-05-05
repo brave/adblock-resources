4a9a03b6b56afcf313c9ab45b4f5cd264e0ad086
ryanbr <rbrown@brave.com>
2025-05-05 23:58:15 +0000

    Update

--- resources/brave-fix.js
+++ resources/brave-fix.js
@@ -3,12 +3,47 @@
 delete Navigator.prototype.brave
 delete window.navigator.brave
 
-const brands = navigator?.userAgentData?.brands;
-if (brands) {
-  brands.forEach((item) => {
-    if (item?.brand?.toLowerCase() === "brave") {
-      item.brand = "Google Chrome";
+const spoofBrands = (items) => {
+  return items.map((item) => {
+    if (item?.brand?.toLowerCase() === 'brave') {
+      return { ...item, brand: 'Google Chrome' };
     }
-  })
-  Object.defineProperty(NavigatorUAData.prototype, "brands", { value: brands });
+    return item;
+  });
+};
+
+let originalBrands = navigator?.userAgentData?.brands;
+if (originalBrands) {
+  try {
+    const spoofedBrands = spoofBrands(originalBrands);
+    Object.defineProperty(NavigatorUAData.prototype, 'brands', {
+      value: spoofedBrands
+    });
+  } catch {}
+}
+
+const originalGetHighEntropyValues =
+  NavigatorUAData?.prototype?.getHighEntropyValues;
+
+if (originalGetHighEntropyValues) {
+  try {
+    Object.defineProperty(NavigatorUAData.prototype, 'getHighEntropyValues', {
+      value: async function getHighEntropyValues(hints) {
+        const results = await originalGetHighEntropyValues.call(
+          navigator.userAgentData,
+          hints
+        );
+        for (const key of Object.keys(results)) {
+          if (key === 'brands' || key === 'fullVersionList') {
+            results[key] = spoofBrands(results[key]);
+          }
+        }
+        return results;
+      }
+    });
+
+    Object.defineProperty(NavigatorUAData.prototype.getHighEntropyValues, 'toString', {
+      value: () => 'function getHighEntropyValues() { [native code] }'
+    });
+  } catch {}
 }
